722b1e2d69b6205c6eaf7f38b858a4e64a5f78b1
alwaysblue <zuo819839662@sina.com>
2017-10-10 09:45:07 +0800

    init

--- app/src/main/java/com/example/zuo81/zztt/ADActivity.java
+++ app/src/main/java/com/example/zuo81/zztt/ADActivity.java
@@ -38,7 +38,6 @@ public class ADActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_ad);
